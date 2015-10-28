bd07307042a14ceb0c97ba08545a502cfe88156e
venancio.junior <grilocolportor@yahoo.com>
2015-10-28 14:31:27 -0200

    update main Activity

--- app/src/main/java/avs/org/go/MainActivity.java
+++ app/src/main/java/avs/org/go/MainActivity.java
@@ -1,7 +1,13 @@
 package avs.org.go;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.List;
+
+import avs.org.go.dominio.User;
+import avs.org.go.repository.UserRepository;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent;
+
+        UserRepository userRepository = new UserRepository(this);
+        List<User> userList = userRepository.getUser();
+        if(userList.isEmpty()){
+            intent = new Intent(this, Login.class);
+            startActivity(intent);
+        }else{
+            intent = new Intent(this, Go.class);
+            startActivity(intent);
+        }
     }
 }
