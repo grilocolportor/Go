4bc3d166cbf438b96700d061c8323103d962ec41
venancio.junior <grilocolportor@yahoo.com>
2015-10-28 17:10:47 -0200

    util package

---
+++ app/src/main/java/avs/org/go/Util/Internet.java
@@ -0,0 +1,7 @@
+package avs.org.go.util;
+
+/**
+ * Created by venancio.junior on 28/10/2015.
+ */
+public class Internet {
+}
---
+++ app/src/main/java/avs/org/go/Util/System.java
@@ -0,0 +1,7 @@
+package avs.org.go.Util;
+
+/**
+ * Created by venancio.junior on 28/10/2015.
+ */
+public class System {
+}
