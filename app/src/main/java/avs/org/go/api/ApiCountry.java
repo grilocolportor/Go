29f1447d71f314bff0d492c2cc0638b2ac72c737
venancio.junior <grilocolportor@yahoo.com>
2015-11-26 16:57:55 -0200

    fix country

--- app/src/main/java/avs/org/go/api/ApiCountry.java
+++ app/src/main/java/avs/org/go/api/ApiCountry.java
@@ -4,7 +4,6 @@ import avs.org.go.dominio.Country;
 import retrofit.Call;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
-import retrofit.http.GET;
 import retrofit.http.POST;
 
 /**
@@ -14,6 +13,6 @@ public interface ApiCountry {
 
     @FormUrlEncoded
     @POST("package/ctrl/CtrlCountry.php")
-    Call<Country> getCountry( @Field("method") String method);
+    Call<Country> getCountry( @Field("method") String method, @Field("codCountry") String codCountry);
 
 }
