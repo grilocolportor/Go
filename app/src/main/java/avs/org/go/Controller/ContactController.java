c8218b6360a7f4a7b5560d55752aceb24d3d5b90
venancio.junior <grilocolportor@yahoo.com>
2015-12-21 15:22:43 -0300

    inicio do processo de contatos do usuasrio 6

--- app/src/main/java/avs/org/go/Controller/ContactController.java
+++ app/src/main/java/avs/org/go/Controller/ContactController.java
@@ -1,26 +1,18 @@
 package avs.org.go.Controller;
 
-import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
-import avs.org.go.R;
 import avs.org.go.api.ApiContact;
-import avs.org.go.api.ApiCountry;
 import avs.org.go.dominio.Contact;
-import avs.org.go.dominio.Country;
-import avs.org.go.dominio.Device;
 import avs.org.go.repository.ContactRepository;
-import avs.org.go.repository.CountryRespository;
 import avs.org.go.util.Constantes;
 import avs.org.go.util.ContactDeserializer;
-import avs.org.go.util.CountryDeserializer;
 import retrofit.Call;
 import retrofit.Callback;
 import retrofit.GsonConverterFactory;
@@ -46,6 +38,28 @@ public class ContactController {
         for(Contact contact : contactList){
             findContact(contact);
         }
+    }
+
+    public void findContacts(List<Contact> contact) {
+
+        Gson gson = new GsonBuilder().registerTypeAdapter(Contact.class,new ContactDeserializer() ).create();
+
+        Retrofit retrofit = new Retrofit
+                .Builder()
+                .baseUrl(API)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        contactApi = retrofit.create(ApiContact.class);
+
+        Call<Contact> call = contactApi.getContact("save-contacts", new Gson().toJson(contact));
+        call.enqueue(new Callback<Contact>() {
+            @Override
+            public void onResponse(Response<Contact> response, Retrofit retrofit) {}
+            @Override
+            public void onFailure(Throwable t) {
+                Log.i(TAG, "Error SAVE CONTACTS: " + t.getMessage());
+            }
+        });
     }
 
 
